65ea5c737555022ec1f17f009f2109802994ff27
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-03-02 21:17:20 +0200

    Add Home Page Component

---
+++ src/HomePage/HomePage.Componenet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../Components/Directory/Directory.Componenet';
+import './HomePage.Componenet.scss';
+
+const HomePage = () => {
+    return ( 
+        <div className="homepage">
+            <Directory />
+        </div>
+     );
+}
+ 
+export default HomePage;
